e11189f6b428b6100b14c68a43adb3c0101c6ecd
Babayev2326254 <f.babayev1389@gmail.com>
2024-09-01 16:35:40 +0400

    eight commit

---
+++ test/test.py
@@ -0,0 +1,18 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+
+# @app.route('/hello')
+# def home():
+#     return "salam, Uşaqlar"
+
+# @app.route('/hello/<name>')
+# def hello(name):
+#     return f"Salam, {name}!"
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True,port=5055)
+    
